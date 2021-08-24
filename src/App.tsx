bd39d6ef19e95ea6a4db2a5d232d7c0275f6d39c
airtonlimajr <airtonlimajr@outlook.com>
2021-08-24 08:09:48 -0300

    Aula 2.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
+import { Home } from './pages/Home';
 
 function App() {
   return (
-<h1>Teste</h1>
+    <Home/>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { Button } from './components/Button'
-import './services/firebase'
+import { Button } from './components/Button';
+import './services/firebase';
+import './styles/global.scss';
 
 
 ReactDOM.render(
